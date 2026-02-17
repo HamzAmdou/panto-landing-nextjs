a543d3b42bd9f45d7ae3575c012ff02b25bf93de
Hamza AMDOUNI <amdounii.hamza@gmail.com>
2026-02-17 05:01:26 +0100

    feat: add Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import { BestSelling } from "@/components/sections/BestSelling/BestSelling";
 import { Experience } from "@/components/sections/Experience/Experience";
 import { Materials } from "@/components/sections/Materials/Materials";
 import { Testimonials } from "@/components/sections/Testimonials/Testimonials";
+import { Footer } from "@/components/sections/Footer/Footer";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <WhyChoosingUs />
-      <BestSelling />
-      <Experience />
-      <Materials />
-      <Testimonials />
-    </main>
+    <>
+      <main>
+        <Hero />
+        <WhyChoosingUs />
+        <BestSelling />
+        <Experience />
+        <Materials />
+        <Testimonials />
+      </main>
+      <Footer />
+    </>
   );
 }
